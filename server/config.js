6745eeb8b8f2207c5991f29f4edb267e7fba1ce7
blue <38808453+61ue@users.noreply.github.com>
2018-06-19 23:58:52 +0800

    服务器

---
+++ server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'mysql.host': 'localhost',
+    'mysql.user': 'root',
+    'mysql.password': '',
+    'mysql.database': 'test',
+    'session.secret': 'i.m.2.hungry',
+    'auth.qq.app_id': '101484370',
+    'dir.uploads': './public',
+    'py.foodreg.path': '?',
+    'py.foodreg.dir': '?'
+}
